670fe08bd60bd51f7d8c3bcebc1866e670d64a67
Nishanjali Kamalendran <nishanjali.kamal@gmail.com>
2025-04-16 20:33:04 +0530

    nav bar updated

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -36,7 +36,6 @@ const Navbar = ({ scrolled, menuOpen, toggleMenu }) => {
         </button>
         <nav className={menuOpen ? 'mobile-active' : ''}>
           <ul>
-            <li><a href="#about">About</a></li>
             <li><a href="#projects">Projects</a></li>
             <li><a href="#experience">Experience</a></li>
             <li><a href="#education">Education</a></li>
